1e23bbc2901772828b5271f83a0481e357d0333f
leonardo.pol <leonardo.pol@vodafone.com>
2023-09-27 18:35:50 +0200

    fix(docker): change wallet service port from 6000 to 5000 (never use again 6000 port: ERR_UNSAFE_PORT 💀) | remove healthcheck from init script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,16 +4,4 @@ docker-compose -f docker/docker-compose.yml up -d
 
 echo "Init Containers! 🐋"
 
-echo "Checking Wallet Microservice Status..."
-sleep 5
-
-url="http://localhost:6000/ok"
-http_status=$(curl -o /dev/null -I -sw "%{http_code}" $url)
-if [ $http_status -eq 200 ]; then
-    echo "Wallet Microservice is OK! ✅"
-else
-    echo "Wallet Microservice Failed! ❌"
-fi
-
 echo "Setup Completed! 🚀"
-echo "Have fun! 🦄"
